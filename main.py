1ce771464bc37b1bc2f31609077704a1a0f9baba
2emiln <2emiln@gmail.com>
2025-08-28 11:09:31 +0200

    Changed extract path

--- main.py
+++ main.py
@@ -1,5 +1,12 @@
 from etl.extract import extract
 from etl.transform import transform
+from etl.load import load_to_sql
 
-df = extract()
-print(df.head())
+def main():
+    df = extract("data/raw/sales.csv")
+    df_t = transform(df)
+    load_to_sql(df_t)  # skriver till data/sales_per_city.db, tabell sales_per_city
+    print("Klar.")
+
+if __name__ == "__main__":
+    main()
